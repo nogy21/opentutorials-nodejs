0bf44eb78abf2f748f6b96165178f72b131d31b3
Yong Lee <nogy21@gmail.com>
2022-07-09 23:03:23 +0900

    쿠키 생성, 읽기
    사용 목적: 1. 인증, 2. 개인화, 3. 트래킹
    http 응답 메세지의 헤더에 쿠키 값 설정

---
+++ nodejs/cookie.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const cookie = require('cookie');
+http
+  .createServer((req, res) => {
+    console.log(req.headers.cookie);
+    const cookies =
+      req.headers.cookie === undefined ? {} : cookie.parse(req.headers.cookie);
+    console.log(cookies.yummy_cookie);
+    res.writeHead(200, {
+      'Set-Cookie': ['yummy_cookie = choco', 'tasty_cookie = strawberry'],
+    });
+    res.end('Cookie!!');
+  })
+  .listen(3000);
