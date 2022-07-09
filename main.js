d307beb469fb7828ea295c5cd6f6bf32bbc81c08
Yong Lee <nogy21@gmail.com>
2022-07-09 21:02:13 +0900

    미들웨어 만들기

--- main.js
+++ main.js
@@ -11,69 +11,70 @@ const compression = require('compression');
 app.use(express.urlencoded({ extended: false }));
 // comepression
 app.use(compression());
-
-// route, routing
-app.get('/', (request, response) => {
+// application-level middleware
+app.get('*', (req, res, next) => {
   fs.readdir('./data', function (error, filelist) {
-    const title = 'Welcome';
-    const description = 'Hello, Node.js';
-    const list = template.list(filelist);
-    const html = template.HTML(
-      title,
-      list,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>`
-    );
-    response.send(html);
+    req.list = filelist;
+    next();
   });
 });
 
+// route, routing
+app.get('/', (request, response) => {
+  const title = 'Welcome';
+  const description = 'Hello, Node.js';
+  const list = template.list(req.filelist);
+  const html = template.HTML(
+    title,
+    list,
+    `<h2>${title}</h2>${description}`,
+    `<a href="/create">create</a>`
+  );
+  response.send(html);
+});
+
 app.get('/page/:pageId', (req, res) => {
-  fs.readdir('./data', function (error, filelist) {
-    const filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      const title = req.params.pageId;
-      const sanitizedTitle = sanitizeHtml(title);
-      const sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1'],
-      });
-      const list = template.list(filelist);
-      const html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        `<a href="/create">create</a>
+  const filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
+    const title = req.params.pageId;
+    const sanitizedTitle = sanitizeHtml(title);
+    const sanitizedDescription = sanitizeHtml(description, {
+      allowedTags: ['h1'],
+    });
+    const list = template.list(req.filelist);
+    const html = template.HTML(
+      sanitizedTitle,
+      list,
+      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+      `<a href="/create">create</a>
               <a href="/update/${sanitizedTitle}">update</a>
               <form action="/delete" method="post">
                 <input type="hidden" name="id" value="${sanitizedTitle}">
                 <input type="submit" value="delete">
               </form>`
-      );
-      res.send(html);
-    });
+    );
+    res.send(html);
   });
 });
 
 app.get('/create', (req, res) => {
-  fs.readdir('./data', function (error, filelist) {
-    const title = 'WEB - create';
-    const list = template.list(filelist);
-    const html = template.HTML(
-      title,
-      list,
-      `<form action="/create" method="post">
-          <p><input type="text" name="title" placeholder="title"></p>
-          <p>
-              <textarea name="description" placeholder="description"></textarea>
-          </p>
-          <p>
-              <input type="submit">
-          </p>
-        </form>`,
-      ''
-    );
-    res.send(html);
-  });
+  const title = 'WEB - create';
+  const list = template.list(req.filelist);
+  const html = template.HTML(
+    title,
+    list,
+    `<form action="/create" method="post">
+      <p><input type="text" name="title" placeholder="title"></p>
+      <p>
+          <textarea name="description" placeholder="description"></textarea>
+      </p>
+      <p>
+          <input type="submit">
+      </p>
+    </form>`,
+    ''
+  );
+  res.send(html);
 });
 
 app.post('/create', (req, res) => {
@@ -106,30 +107,28 @@ app.post('/create', (req, res) => {
 });
 
 app.get('/update/:pageId', (req, res) => {
-  fs.readdir('./data', (error, filelist) => {
-    const filteredId = path.parse(req.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
-      const title = req.params.pageId;
-      const list = template.list(filelist);
-      const html = template.HTML(
-        title,
-        list,
-        `
-          <form action="/update" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-            <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-            </p>
-            <p>
-                <input type="submit">
-            </p>
-          </form>
-          `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-      );
-      res.send(html);
-    });
+  const filteredId = path.parse(req.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', (err, description) => {
+    const title = req.params.pageId;
+    const list = template.list(req.filelist);
+    const html = template.HTML(
+      title,
+      list,
+      `
+      <form action="/update" method="post">
+        <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+        <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+        </p>
+        <p>
+            <input type="submit">
+        </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+    );
+    res.send(html);
   });
 });
 
