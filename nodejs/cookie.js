c166714c05e8982ad356754c30b1e92ec701f36f
nogy21 <nogy21@gmail.com>
2022-07-10 20:35:35 +0900

    Session cookie & Permanent cookie

--- nodejs/cookie.js
+++ nodejs/cookie.js
@@ -1,14 +1,18 @@
-const http = require('http');
-const cookie = require('cookie');
-http
-  .createServer((req, res) => {
-    console.log(req.headers.cookie);
-    const cookies =
-      req.headers.cookie === undefined ? {} : cookie.parse(req.headers.cookie);
-    console.log(cookies.yummy_cookie);
-    res.writeHead(200, {
-      'Set-Cookie': ['yummy_cookie = choco', 'tasty_cookie = strawberry'],
-    });
-    res.end('Cookie!!');
-  })
-  .listen(3000);
+const http = require("http");
+const cookie = require("cookie");
+http.createServer((req, res) => {
+	console.log(req.headers.cookie);
+	const cookies =
+		req.headers.cookie === undefined
+			? {}
+			: cookie.parse(req.headers.cookie);
+	console.log(cookies.yummy_cookie);
+	res.writeHead(200, {
+		"Set-Cookie": [
+			"yummy_cookie = choco",
+			"tasty_cookie = strawberry",
+			"PermanentCookie = lemon; Max-Age = ${60 * 60 * 24 * 30}",
+		],
+	});
+	res.end("Cookie!!");
+}).listen(3000);
