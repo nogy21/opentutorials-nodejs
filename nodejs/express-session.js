8cba265506b66372c10489d7a15b644009155955
Yong Lee <nogy21@gmail.com>
2022-07-13 23:19:56 +0900

    express-session의 session store

--- nodejs/express-session.js
+++ nodejs/express-session.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const parseurl = require('parseurl');
 const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 const app = express();
 
@@ -8,9 +9,10 @@ app.use(
   // session store instance: default - MemoryStore instance => memory는 휘발성이기에 서버가 꺼지면 데이터 out
   session({
     // session middleware로 req 객체 프로퍼티로 session 생성
-    secret: 'keyboard cat', // 공개되어서는 안되는 비밀 키
+    secret: 'asdfzxcv!@#$%ASDF', // 공개되어서는 안되는 비밀 키
     resave: false, // session data가 바뀌기 전 까지는 저장하지 않는다
     saveUninitialized: true, // session이 필요하기 전 까지는 초기화하지 않는다
+    store: new FileStore(),
   })
 );
 
