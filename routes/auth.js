6ea01a8ec228afdeae92d8ff31b77555c2ddd5a3
Yong Lee <nogy21@gmail.com>
2022-07-13 23:36:56 +0900

    session login - 로그인 세션 구현

--- routes/auth.js
+++ routes/auth.js
@@ -7,13 +7,19 @@ const fs = require('fs');
 const template = require('../lib/template');
 // const { checkCookie } = require('../nodejs/cookieFunc');
 
+const authData = {
+  email: 'nogy21@gmail.com',
+  password: '1111', // 비밀번호는 소스코드 바깥 쪽에 위치해야 함. 또한, hash나 암호 알고리즘 적용 필수
+  nickname: 'yong',
+};
+
 router.get('/login', (req, res) => {
   const title = 'WEB - login';
   const list = template.list(req.list);
   const html = template.HTML(
     title,
     list,
-    `<form action="/topic/login" method="post">
+    `<form action="/auth/login" method="post">
       <p><input type="text" name="email" placeholder="email"></p>
       <p><input type="password" name="pwd" placeholder="password"></p>
       <p>
@@ -26,17 +32,18 @@ router.get('/login', (req, res) => {
   // }
 });
 
-/*
-router.post('/create', (req, res) => {
-  if (checkCookie(req, res)) {
-    const post = req.body;
-    const title = post.title;
-    const description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      res.redirect(`/topic/${title}`);
-    });
+router.post('/login', (req, res) => {
+  const post = req.body;
+  const email = post.email;
+  const password = post.pwd;
+  if (email === authData.email && password === authData.password) {
+    res.send('Welcome!');
   }
+  res.send('Who?');
+  // res.redirect(`/topic/${title}`);
 });
+
+/*
 
 router.get('/update/:pageId', (req, res) => {
   if (checkCookie(req, res)) {
